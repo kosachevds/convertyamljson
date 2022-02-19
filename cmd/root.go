cd45911fe07a88a2426b55a8d52263de1f9e60d7
Kosachev Daniil <kos-dan@mail.ru>
2022-02-19 17:29:10 +0300

    Use 'convert' command as default

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,10 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
+
+var defaultCmd = convertCmd
 
 var rootCmd = &cobra.Command{
 	Use: "convertyamljson",
@@ -14,8 +17,18 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	insertIfNoCommand(rootCmd, defaultCmd)
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+func insertIfNoCommand(root, defaultCmd *cobra.Command) {
+	defaultCmd.Short += " (call by default)"
+	cmd, _, err := root.Find(os.Args[1:])
+	if err != nil && cmd == root && cmd.Flags().Parse(os.Args[1:]) != pflag.ErrHelp {
+		newArgs := append([]string{defaultCmd.Name()}, os.Args[1:]...)
+		root.SetArgs(newArgs)
 	}
 }
